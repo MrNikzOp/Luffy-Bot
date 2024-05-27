4c88272f7247f22cb4c331d2cdf75abb0ece612f
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-28 01:27:53 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -2,8 +2,8 @@ import os
 from plugins.functions.display_progress import progress_for_pyrogram, humanbytes
 from plugins.config import Config
 from plugins.script import Translation
-from pyrogram import Client, types
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, types   
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
 from plugins.database.database import db
 import logging
 logging.basicConfig(level=logging.DEBUG,
