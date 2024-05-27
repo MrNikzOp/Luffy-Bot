8e28d7bb4b0d7e23b72382b037406eb1c5877229
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 19:47:34 +0530

    Update invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -4,6 +4,7 @@ from plugins.config import Config
 from plugins.script import Translation
 from plugins.functions.forcesub import handle_force_subscribe
 
+PIC = "https://telegra.ph/file/72b1efaa44944d2b9e1b9.jpg"
 
 # Invite command handler
 @Client.on_message(filters.private & filters.command("invite"))
@@ -13,7 +14,9 @@ async def about(client, message):
       if fsub == 400:
         return
   #  await message.reply_photo(photo)  
-    await message.reply_text(
-        text=Translation.ABOUT_TXT,
+    await message.reply_photo(
+        photo=PIC,
+        caption=Translation.ABOUT_TXT,
         disable_web_page_preview=True,
-        reply_markup=BUTTONS)
+        reply_markup=BUTTONS
+    )
