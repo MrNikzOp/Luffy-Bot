fdc97599b0f422355c5f6da361ee05c3a5b95e3e
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 12:14:50 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -31,3 +31,5 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
+    elif "close" in update.data:
+        await update.message.delete(True)
