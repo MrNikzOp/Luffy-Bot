60e4ed4b629098b2ec1458aefe47d0b3913a6c54
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 20:03:52 +0530

    Update invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -9,10 +9,6 @@ PIC = "https://telegra.ph/file/72b1efaa44944d2b9e1b9.jpg"
 # Invite command handler
 @Client.on_message(filters.private & filters.command("invite"))
 async def ref(client, message):
-    if Config.CHANNEL:
-      fsub = await handle_force_subscribe(client, message)
-      if fsub == 400:
-        return
   #  await message.reply_photo(photo)  
     await message.reply_photo(
         photo=PIC,
