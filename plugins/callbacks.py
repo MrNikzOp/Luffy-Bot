373012a23f84220a99338860d1039dd767736dfb
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 09:49:44 +0530

    Create callbacks.py

---
+++ plugins/callbacks.py
@@ -0,0 +1,38 @@
+import os
+from plugins.functions.display_progress import progress_for_pyrogram, humanbytes
+from plugins.config import Config
+from plugins.dl_button import ddl_call_back
+from plugins.button import youtube_dl_call_back
+from plugins.settings.settings import OpenSettings
+from plugins.script import Translation
+from pyrogram import Client, types
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.database.database import db
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+
+@Client.on_callback_query()
+async def button(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+      
