384411fffa626d90cd3c620622d11c448e62a739
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 10:06:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
       if fsub == 400:
         return
     await update.reply_text(
-        text=Translation.PREMIUM_TEXT.format(update.from_user.mention),
+        text=Translation.DETAILS_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.PREMIUM_BUTTONS
     )
