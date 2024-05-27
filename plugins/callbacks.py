b37b22fd7a43f2543b480babd3041835d7df61ff
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-28 01:16:40 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -25,7 +25,11 @@ async def button(bot, update):
             reply_markup=Translation.PREMIUM_BUTTONS,
             disable_web_page_preview=True
         )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Translation.ABOUT_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
     elif "close" in update.data:
         await update.message.delete(True)
-    else:
-        await update.message.delete()  
