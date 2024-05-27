8dbcd33b7b2b0d74e90ba9b3eb09341e41b36437
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-28 01:03:57 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -27,7 +27,7 @@ async def button(bot, update):
         )
      elif update.data == "about":
         await update.message.edit_text(
-            text=Translation.PAY_TEXT,
+            text=Translation.ABOUT_TEXT,
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
