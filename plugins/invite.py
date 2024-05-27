703a59b86a6a9fc96a19a84a665395337df9e100
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 19:43:48 +0530

    Update invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -1,1 +1,5 @@
-
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+from plugins.config import Config
+from plugins.script import Translation
+from Luffy.forcesub import handle_force_subscribe
