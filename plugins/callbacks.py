afb0d538b4fb22a2d717f530f5ca4315ece783e1
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 11:14:28 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -10,9 +10,6 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-@Client.on_callback_query(filters.regex("close"))
-async def cancel(client, callback_query):
-    await callback_query.message.delete()
 
 @Client.on_callback_query()
 async def button(bot, update):
@@ -34,4 +31,5 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
-      
+      elif "close" in update.data:
+        await update.message.delete(True)
