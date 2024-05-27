aaf4e8abe13a2d0c60b555799fda76e49e653dfe
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 19:45:29 +0530

    Update invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -2,4 +2,18 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 from plugins.config import Config
 from plugins.script import Translation
-from Luffy.forcesub import handle_force_subscribe
+from plugins.functions.forcesub import handle_force_subscribe
+
+
+# Invite command handler
+@Client.on_message(filters.private & filters.command("invite"))
+async def about(client, message):
+    if Config.CHANNEL:
+      fsub = await handle_force_subscribe(client, message)
+      if fsub == 400:
+        return
+  #  await message.reply_photo(photo)  
+    await message.reply_text(
+        text=Translation.ABOUT_TXT,
+        disable_web_page_preview=True,
+        reply_markup=BUTTONS)
