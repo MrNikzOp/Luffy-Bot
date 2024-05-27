d0eb76326afb36e832bdc425c2cfa4aeb0683414
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 13:25:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,9 +31,8 @@ async def start(bot, update):
       fsub = await handle_force_subscribe(bot, update)
       if fsub == 400:
         return
-    await update.reply_photo(
-        photo=PIC,
-        caption=Translation.START_TEXT.format(update.from_user.mention),
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
     )
