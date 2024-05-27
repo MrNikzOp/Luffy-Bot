753e2737f9566140698466f68beb931e1c6a4a40
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-27 14:58:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,7 +32,9 @@ async def start(bot, update):
       if fsub == 400:
         return
     await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
+        photo=PIC,
+        caption=Translation.START_TEXT,
+       # text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
     )
