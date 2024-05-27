e4686324d716d08537dde550bf6d64a179922166
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-28 00:33:29 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -31,7 +31,7 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "pay":
+    elif update.data == "payment":
         await update.message.edit_text(
             text=Translation.QR_TEXT,
             reply_markup=Translation.ABOUT_BUTTONS,
